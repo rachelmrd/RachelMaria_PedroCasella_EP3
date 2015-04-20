75b0ac2933b86a6e0d9b4d8b15dfb1c7055931e6
rachelmrd <rachel.mrd@hotmail.com>
2015-04-19 23:01:03 -0300

    Edições
    comentários no próprio código

--- ep3.py
+++ ep3.py
@@ -6,148 +6,187 @@ Created on Wed Apr 15 16:52:19 2015
 """
 
 #EP3: Programa de Dieta Semanal 
-
-import matplotlib as plt
+import matplotlib as plt #para plotar os gráficos ideais do usuário
 
 
 #------------------------------------------------------------------------------
 '''
-importando a biblioteca de alimentos
+importando a biblioteca de alimentos como lista
 '''
 
 def lista_alimentos(nome_do_arquivo="alimentos.csv"):
     text = open(nome_do_arquivo, 'r+', encoding = 'utf-8' )
-    read = text.readlines() # lista suja
-    lista_alimentos =[] # lista limpa
-    for linha in read:                  #joga todas as palavras na lista limpa
+    read = text.readlines() #lista suja
+    lista_alimentos =[] #lista limpa
+    for linha in read:   #joga todas as palavras na lista limpa
         y = linha.strip().lower()
         if len(y) > 0:  
-            lista_alimentos.append(y)        #adiciona as palavras a nova lista
+            lista_alimentos.append(y)  #adiciona as palavras a nova lista
     return lista_alimentos
     
     
 lista_alimentos('alimentos.csv')
 
-#------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
 '''
-importanto a lista fornecida pelo execel
+importando a lista do usuário fornecida pelo excel
 '''
 
 def lista_usuário(nome_do_arquivo="usuario.csv"):
     text = open(nome_do_arquivo, 'r+', encoding = 'utf-8' )
-    read = text.readlines() # lista suja
-    lista_usuario =[] # lista limpa
-    for linha in read:                  #joga todas as palavras na lista limpa
+    read = text.readlines()[3:] #lista suja, pula as linhas de instrução
+    lista_usuario =[] #lista limpa
+    
+    for linha in read:   #joga todas as palavras na lista limpa
         y = linha.strip().lower()
         if len(y) > 0:  
-            lista_usuario.append(y)         #adiciona as palavras a nova lista
+            lista_usuario.append(y) #adiciona as palavras à nova lista
     return lista_usuario
     
     
-lista_usuário('usuario.csv')
+print(lista_usuário('usuario.csv'))
+
 
 #------------------------------------------------------------------------------
-
 '''
-criação do dicionario dos alimetos da biblioteca
+criação do dicionario dos alimentos (caracteristicas) da biblioteca
 '''
 
 alimentos={} #criando um dicionario vazio
 
-for i in lista_alimentos: #atribuindo as minhas comidas valores
- alimento_caracteristica=i.split(',')
+for i in lista_alimentos('alimentos.csv'): #atribuindo valores às comidas
+ alimento_caracteristica = i.split(',')
  chave = alimento_caracteristica[0]
  valores = alimento_caracteristica[1:]
  alimentos[chave] = valores
  
-print(alimentos)
-print(alimentos['banana da terra'][1]) 
+
+
+#TESTES:
+#print(alimentos)       
+#print(alimentos['banana da terra'][1]) 
+
 
 #------------------------------------------------------------------------------
+'''
+fórmulas: calorias necessárias
+'''
 
-
-
-
-
-
-
-
-
-
-
-#FÓRMULAS
-#CALORIAS NECESSÁRIAS - Rachel
-def calorias_necessarias():
-    altura = keys.entrada_usuario(ALTURA)
-    peso = keys.entrada_usuario(PESO (kg))
-    idade = keys.entrada_usuario(IDADE)
-    sexo = keys.entrada_usuario(SEXO)
+def calorias_ideais():
+    text = open('usuario.csv', 'r+', encoding = 'utf-8' )
+    carac = text.readlines()[1:2] #lista suja, pega as linhas de instrução
+    carac_usuario =[]  #lista limpa
     
-    if sexo == "M":
-        return (88,36+(13,4*peso)+(4,8*altura)-(5,7*idade))
+    for linha in carac:  #joga todas as palavras na lista limpa
+        y = linha.strip().lower()
+        if len(y) > 0:  
+            carac_usuario.append(y)  #adiciona as palavras a nova lista
+    
+    
+    #TRANSFORMANDO AS INFORMAÇÕES EM ELEMENTOS DE LISTA
+    carac_usuario = carac_usuario[0].strip().split(",")
+    
+
+    #IDENTIFICAÇÃO DE CARACTERÍSTICAS DO USUÁRIO
+    #chave = carac_usuario[0]
+    altura = carac_usuario[4]
+    list(float(altura))
+    peso = carac_usuario[2]
+    list(float(peso))
+    idade = carac_usuario[1]
+    list(float(idade))
+    sexo = carac_usuario[3]
+    
+    #DEFINIÇÃO DAS CALORIAS DIÁRIAS DO USUÁRIO
+    if sexo == "m":
+        return (88,36+(13,4*peso)+(4,8*altura*100)-(5,7*idade))
     else:
         return (447,6+(9,2*peso)+(3,1*altura)-(4,3*idade))
         
+print(calorias_ideais())
+ 
+ 
+#------------------------------------------------------------------------------
+'''
+fórmulas: grau de atividade física
+'''
 
-#GRAU DE ATIVIDADE FISICA - Rachel
 def fator_atividade():
-    fator = keys.entrada_usuario()
+    text = open('usuario.csv', 'r+', encoding = 'utf-8' )
+    carac = text.readlines()[1:2] #lista suja, pega as linhas de instrução
+    carac_usuario =[]  #lista limpa
+    
+    for linha in carac:  #joga todas as palavras na lista limpa
+        y = linha.strip().lower()
+        if len(y) > 0:  
+            carac_usuario.append(y)  #adiciona as palavras a nova lista
+    
+    
+    #TRANSFORMANDO AS INFORMAÇÕES EM ELEMENTOS DE LISTA
+    carac_usuario = carac_usuario[0].strip().split(",")
+    fator = carac_usuario[5]
     
     if fator == "minimo":
-        energia = (calorias_necessarias*1,2)
+        return (calorias_ideais*1,2)
         
     elif fator == "baixo":
-        energia = (calorias_necessarias*1,375)
+        return (calorias_ideais*1,375)
         
     elif fator == "medio":
-        energia = (calorias_necessarias*1,55)
+        return (calorias_ideais*1,55)
         
     elif fator == "alto":
-        energia = (calorias_necessarias*1,725)
+        return (calorias_ideais*1,725)
         
     else:  #fator == muito alto
-        energia = (calorias_necessarias*1,9)
+        return (calorias_ideais*1,9)
 
-
-    
-    
-
-"""
-Distribuições  (por Heloísa Guarita (veja em README))
+  
+#------------------------------------------------------------------------------
+'''
+distribuições de necessidades por Heloísa Guarita (veja em README)
 
 Calórica
-
 - Proteínas = 20% das calorias
 - Carboidratros = 60%
 - Gorduras = 20%
+'''
 
+def proteinas_ideais(fator_atividade):
+    return fator_atividade*0,2
+  
+  
+def carboidratos_ideias(fator_atividade):
+    return fator_atividade*0,6
 
+  
+def gorduras_ideias(fator_atividade):
+    return fator_atividade*0,2
+ 
+ 
+'''
 Refeições
-
 - Café da manhã: 20% das calorias diárias
 - Lanche da manhã: 5%
 - Almoço: 30%
 - Lanche da tarde: 15%
 - Jantar: 20%
 - Ceia: 10%
-"""
+'''
+
+def dieta(calorias_ideais):
+    cafe = calorias_ideais*0.2
+    lanche_manha = calorias_ideais*0.05
+    almoco = calorias_ideais*0.3
+    lanche_tarde = calorias_ideais*0.15
+    jantar = calorias_ideais*0.2
+    ceia = calorias_ideais*0.1
+
+    return (cafe+lanche_manha+almoco+lanche_tarde+jantar+ceia)
 
 
-def proteinas_ideais(energia):
-    return energia*0,2
-  
-  
-def carboidratos_ideias(energia):
-    return energia*0,6
-
-  
-def gorduras_ideias(energia):
-    return energia*0,2
-    
-    
-
-
+#------------------------------------------------------------------------------
 
 
 
@@ -156,15 +195,3 @@ def gorduras_ideias(energia):
 
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
