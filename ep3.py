e00d94ed2359ac45ed8c4b1e4069f3cf076035a6
Rachel Maria <rachel.mrd@hotmail.com>
2015-04-21 23:27:13 -0300

    Gráficos
    Mudança dos gráficos e detalhes

--- ep3.py
+++ ep3.py
@@ -4,21 +4,17 @@ Created on Wed Apr 15 16:52:19 2015
 
 @author: Rachel Maria e Pedro Casella - Sala C
 
-Esse programa calculará sua dieta semanal.
+Esse programa calculará uma dieta semanal.
 Dê suas informações e entre com os alimentos consumidos (quantidade em gramas).
 O programa gerará um gráfico diário e um gráfico de IMC ideal.
 
 
-
 """
-
-
 #EP3: Programa de Dieta Semanal 
 
 import primeira_semana as ps
-import plotly.plotly as py
-from plotly.graph_objs import *
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 #------------------------------------------------------------------------------
@@ -27,7 +23,7 @@ importando a biblioteca de alimentos como lista
 '''
 
 def lista_alimentos(nome_do_arquivo="alimentos.csv"):
-    text = open(nome_do_arquivo, 'r+', encoding = 'utf-8' )
+    text = open(nome_do_arquivo, 'r+')
     read = text.readlines() #lista suja
     lista_alimentos =[] #lista limpa
     for linha in read:  #joga todas as palavras na lista limpa
@@ -51,7 +47,7 @@ for i in lista_alimentos('alimentos.csv'): #atribuindo valores às comidas
     alimento_caracteristica = i.split(',')
     chave = alimento_caracteristica[0]
     valores = alimento_caracteristica[1:]
-    alimentos[chave] = valores               #transformação em dicionário
+    alimentos[chave] = valores               #dicionário
  
 
 #------------------------------------------------------------------------------
@@ -60,15 +56,15 @@ input dos alimentos diários do usuário
 cálculo do que foi ingerido
 '''
 
-ps.alimentos_consumidos_primeira_semana()
+ps.alimentos_consumidos_primeira_semana()#importa do arquivo o input do usuário
 
 
 a1 = sum(ps.alimentos_dia_1)
 a2 = sum(ps.alimentos_dia_2)
-a3 = sum(ps.alimentos_dia_3)
-a4 = sum(ps.alimentos_dia_4)
-a5 = sum(ps.alimentos_dia_5)
-a6 = sum(ps.alimentos_dia_6)
+a3 = sum(ps.alimentos_dia_3)           #faz a soma dos alimentos
+a4 = sum(ps.alimentos_dia_4)            #consumidos a cada dia e
+a5 = sum(ps.alimentos_dia_5)            #transforma nas variaveis 
+a6 = sum(ps.alimentos_dia_6)            #utilizadas no gráfico
 a7 = sum(ps.alimentos_dia_7)
 
 
@@ -77,23 +73,23 @@ a7 = sum(ps.alimentos_dia_7)
 plota o gráfico do que foi consumido a cada dia da semana
 '''
 
-def grafico_consumo_semanal(a1,a2,a3,a4,a5,a6,a7):
-    data = Data([
-        Bar(
-            x=['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7'],
-            y=[a1, a2, a3, a4, a5, a6, a7]
-        )
-    ])
-    py.plot(data, filename = 'Gráfico das calorias ingeridas nessa semana')
-    
-        
-grafico_consumo_semanal(a1,a2,a3,a4,a5,a6,a7)  #chama a função gráfico consumo  
+fig = plt.figure()
+
+x = ('Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7')
+y = (a1, a2, a3, a4, a5, a6, a7)
+width = .35
+ind = np.arange(len(y))    #divisão do espaço pelo número de dias(variaveis)
+plt.bar(ind, y, color='lightpink',edgecolor='black',linewidth=1, width = 0.7)
+plt.xticks(ind + width, x)
+fig.autofmt_xdate()    #deixa a escrita na diagonal -->questão de estilo
+
+plt.show()
 
 
 #------------------------------------------------------------------------------
 '''
-fórmulas: calorias ideias a serem consumidas
-input do usuário de suas características
+fórmulas: calorias de acordo com características
+input do usuário
 '''
 
 def calorias_ideais():
@@ -108,31 +104,31 @@ def calorias_ideais():
         return (447.6+(9.2*peso)+(3.1*altura)-(4.3*idade))
  
       
-relaçao_calorica = calorias_ideais()
+relacao_calorica = calorias_ideais()
 
 
 #------------------------------------------------------------------------------
 '''
-fórmulas: grau de atividade física para saber o quanto consumir
+fórmulas: calorias de acordo com grau de atividade física
 '''
-relaçao_calorica = calorias_ideais()
+
 def fator_atividade(relacao_calorica):    
     fator = input("Informe seu fator de atividade\n (mínimo, baixo, médio, alto ou muito alto):  ")    
     
     if fator == "minimo" or "mínimo":
-        return (relaçao_calorica*1.2)
+        return (relacao_calorica*1.2)
         
     elif fator == "baixo":
-        return (relaçao_calorica*1.375)
+        return (relacao_calorica*1.375)
         
     elif fator == "medio" or "médio":
-        return (relaçao_calorica*1.55)
+        return (relacao_calorica*1.55)
         
     elif fator == "alto":
-        return (relaçao_calorica*1.725)
+        return (relacao_calorica*1.725)
         
     else:  #fator == muito alto
-        return (relaçao_calorica*1.9)
+        return (relacao_calorica*1.9)
 
 fator_atividade() 
 ideal = fator_atividade()
@@ -141,25 +137,25 @@ ideal = fator_atividade()
 '''
 plota o gráfico do que foi consumido na semana comparando com o ideal
 '''
+
 semana = (a1+a2+a3+a4+a5+a6+a7)/7
 
-def grafico_consumo_comparado(semana,ideal):
-    data = Data([
-        Bar(
-            x=['Consumido na Semana', 'Consumo ideal'],
-            y=[semana,ideal]
-        )
-    ])
-    py.plot(data, filename = 'Comparação Consumo x Ideal')
-    
-    
-        
-grafico_consumo_comparado(semana,ideal)  #chama a comparação  
+dias = ('Ideal', 'Semana', 'Dia 7', 'Dia 6', 'Dia 5', 'Dia 4', 'Dia 3', 'Dia 2', 'Dia 1')
+y_pos = np.arange(len(dias))
+calorias = ideal, semana, a7, a6, a5, a4, a3, a2, a1
+
+
+plt.barh(y_pos, calorias, align='center', alpha=0.4, color='lightpink')
+plt.yticks(y_pos, dias)
+plt.xlabel('Calorias')
+plt.title('Grafico Semanal - Compare o ideal, a media da semana e cada dia')
+
+plt.show()
 
 
 #------------------------------------------------------------------------------
 '''
-distribuições de necessidades por Heloísa Guarita (veja em README)
+distribuições de necessidades por Heloísa Guarita
 
 Calórica
 - Proteínas = 20% das calorias
@@ -190,33 +186,29 @@ Refeições
 '''
 
 
-cafe = relaçao_calorica*0.2
-lanche_manha = relaçao_calorica*0.05
-almoco = relaçao_calorica*0.3
-lanche_tarde = relaçao_calorica*0.15
-jantar = relaçao_calorica*0.2
-ceia = relaçao_calorica*0.1
-
+cafe = relacao_calorica*0.2
+lanche_manha = relacao_calorica*0.05
+almoco = relacao_calorica*0.3
+lanche_tarde = relacao_calorica*0.15
+jantar = relacao_calorica*0.2
+ceia = relacao_calorica*0.1
 
 
 #------------------------------------------------------------------------------
 '''
-plota o gráfico das divisões em refeições
+plota um gráfico-demonstração de distribuição de refeições
 '''
 
-def grafico_refeicoes(semana,ideal):
-    data = Data([
-        Bar(
-            x=['Café', 'Lanche Manhã', 'Almoço', 'Lanche Tarde', 'Jantar', 'Ceia'],
-            y=[cafe, lanche_manha, almoco, lanche_tarde, jantar, ceia]
-        )
-    ])
-    py.plot(data, filename = 'Calorias por refeição')
-    
-    
-        
-grafico_refeicoes(semana,ideal)  #chama a comparação  
+labels = 'Cafe', 'Lanche1', 'Almoco', 'Lanche2', 'Janta', 'Ceia'
+sizes = [20, 5, 30, 15, 20, 10]
+colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'blue', 'pink']
+explode = (0.05, 0.1, 0.05, 0.1, 0.05, 0.1) #serapação das rfeições
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=90)
+plt.axis('equal') #transforma em círculo
 
+plt.show()
 
+#------------------------------------------------------------------------------
 
 
--- testes do ep3 - rachel.py
+++ testes do ep3 - rachel.py
@@ -4,21 +4,76 @@ Created on Mon Apr 20 15:12:56 2015
 
 @author: Rachel
 """
-import primeira_semana as ps
+
+'''
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 import ep3 as ep
 
+# Example data
+dias = ('Dia 7', 'Dia 6', 'Dia 5', 'Dia 4', 'Dia 3', 'Dia 2', 'Dia 1')
+y_pos = np.arange(len(dias))
+y = ep.a1, ep.a2, ep.a3, ep.a4, ep.a5, ep.a6, ep.a7
+#error = np.random.rand(len(dias))
 
-import plotly.plotly as py
-from plotly.graph_objs import *
+plt.barh(y_pos, y, align='center', alpha=0.4, color = 'lightpink')
+plt.yticks(y_pos, dias)
+plt.xlabel('Calorias Consumidas')
+plt.title('Gráfico Semanal')
 
-def grafico_consumo_comparado(a1,a2,a3,a4,a5,a6,a7):
-    data = Data([
-        Bar(
-            x=['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7'],
-            y=[a1, a2, a3, a4, a5, a6, a7]
-        )
-    ])
-    py.plot(data, filename = 'Gráfico das calorias ingeridas nessa semana')
-    
-        
-grafico_consumo_comprado(a1,a2,a3,a4,a5,a6,a7)  #chama a função gráfico consumo  
+plt.show()
+'''
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+
+a1 = 1523
+a2 = 2156
+a3 = 1658
+a4 = 1793
+a5 = 1500
+a6 = 1632
+a7 = 2001
+ideal = 1500
+
+
+'''
+N = 7
+menMeans   = (a1, a2, a3, a4, a5, a6, a7)
+#womenMeans = (25, 32, 34, 20, 25)
+#menStd     = (2, 3, 4, 1, 2)
+#womenStd   = (3, 5, 2, 3, 3)
+ind = np.arange(N)    # the x locations for the groups
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+p1 = plt.bar(ind, menMeans,   width, color='r')
+
+plt.ylabel('Calorias')
+plt.title('Grafico Semanal')
+plt.xticks(ind+width/2., ('Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7'))
+plt.yticks(np.arange(0,10000,10))
+plt.legend( (p1[0]), ('Men', 'Women') )
+
+plt.show()
+'''
+
+
+fig = plt.figure()
+
+x = ('Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6', 'Dia 7')
+y = (a1, a2, a3, a4, a5, a6, a7)
+width = .35
+ind = np.arange(len(y))    #divisão do espaço pelo número de dias(variaveis)
+plt.bar(ind, y, color='lightpink',edgecolor='black',linewidth=1, width = 0.7)
+plt.xticks(ind + width, x)
+fig.autofmt_xdate()
+
+plt.show()
+
+
+
+
+
+
