e711eb991c0b09ea913208820881e8cf3b48f3dc
pepocasella <pepocasella@gmail.com>
2015-04-22 16:07:56 -0300

    Alterações
    - juntamos a parte de input no mesmo arquivo para otimizar
    - graficos rodando e inputs rodando
    - falta exportar o texto

--- ep3.py
+++ ep3.py
@@ -51,21 +51,96 @@ for i in lista_alimentos('alimentos.csv'): #atribuindo valores às comidas
  
 
 #------------------------------------------------------------------------------
-'''
-input dos alimentos diários do usuário
-cálculo do que foi ingerido
-'''
-
-ps.alimentos_consumidos_primeira_semana()#importa do arquivo o input do usuário
 
 
-a1 = sum(ps.alimentos_dia_1)
-a2 = sum(ps.alimentos_dia_2)
-a3 = sum(ps.alimentos_dia_3)           #faz a soma dos alimentos
-a4 = sum(ps.alimentos_dia_4)            #consumidos a cada dia e
-a5 = sum(ps.alimentos_dia_5)            #transforma nas variaveis 
-a6 = sum(ps.alimentos_dia_6)            #utilizadas no gráfico(consumo)
-a7 = sum(ps.alimentos_dia_7)
+
+def alimentos_consumidos_primeira_semana():
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no primeiro dia:  ")
+        if alimento_usuario == "fim":
+            break
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_1.append(x)
+            
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no segundo dia:  ")
+        if alimento_usuario == "fim":
+            break
+            
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_2.append(x)
+    
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no terceiro dia:  ")
+        if alimento_usuario == "fim":
+            break
+            
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_3.append(x)
+        
+    
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no quarto dia:  ")
+        if alimento_usuario == "fim":
+            break
+            
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_4.append(x)
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no quinto dia:  ")
+        if alimento_usuario == "fim":
+            break
+            
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_5.append(x)
+    
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no sexto dia:  ")
+        if alimento_usuario == "fim":
+            break
+            
+        quantidade = float(input("Quantas gramas?:  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_6.append(x)
+        
+    
+    while True:
+        alimento_usuario = input("Informe os alimentos consumidos no sétimo dia:  ")
+        if alimento_usuario == "fim":
+            break
+        quantidade = float(input("Quantas gramas?  "))
+        quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
+        x = (quantidade_nominal*quantidade)/100
+        alimentos_dia_6.append(x)
+
+#ps.alimentos_consumidos_primeira_semana()#importa do arquivo o input do usuário
+
+alimentos_consumidos_primeira_semana()
+
+a1 = sum(alimentos_dia_1)
+a2 = sum(alimentos_dia_2)
+a3 = sum(alimentos_dia_3)           #faz a soma dos alimentos
+a4 = sum(alimentos_dia_4)            #consumidos a cada dia e
+a5 = sum(alimentos_dia_5)            #transforma nas variaveis 
+a6 = sum(alimentos_dia_6)            #utilizadas no gráfico(consumo)
+a7 = sum(alimentos_dia_7)
 
 
 #------------------------------------------------------------------------------ 
@@ -130,8 +205,8 @@ def fator_atividade(relacao_calorica):
     else:  #fator == muito alto
         return (relacao_calorica*1.9)
 
-fator_atividade() 
-ideal = fator_atividade()   #para uso no gráfico (comparação)
+fator_atividade(relacao_calorica) 
+ideal = fator_atividade(relacao_calorica)   #para uso no gráfico (comparação)
 
 #------------------------------------------------------------------------------  
 '''
--- testes do ep3.py
+++ testes do ep3.py
@@ -4,20 +4,20 @@ Created on Thu Apr 16 08:43:50 2015
 
 @author: Insper
 """
-
+'''
 import primeira_semana as ps
 
 
 #------------------------------------------------------------------------------
-'''
+
 importando a biblioteca de alimentos como lista
 '''
 
 def lista_alimentos(nome_do_arquivo="alimentos.csv"):
-    text = open(nome_do_arquivo, 'r+', encoding = 'utf-8' )
+    text = open(nome_do_arquivo, 'r+')
     read = text.readlines() #lista suja
     lista_alimentos =[] #lista limpa
-    for linha in read:   #joga todas as palavras na lista limpa
+    for linha in read:  #joga todas as palavras na lista limpa
         y = linha.strip().lower()
         if len(y) > 0:  
             lista_alimentos.append(y)  #adiciona as palavras a nova lista
@@ -35,36 +35,10 @@ criação do dicionario dos alimentos com suas caracteristicas
 alimentos={} #criando um dicionario vazio
 
 for i in lista_alimentos('alimentos.csv'): #atribuindo valores às comidas
- alimento_caracteristica = i.split(',')
- chave = alimento_caracteristica[0]
- valores = alimento_caracteristica[1:]
- alimentos[chave] = valores
+    alimento_caracteristica = i.split(',')
+    chave = alimento_caracteristica[0]
+    valores = alimento_caracteristica[1:]
+    alimentos[chave] = valores               #dicionário
  
 
-
-alimentos_dia_1 = []
-
-
-    
-while True:
-   alimento_usuario = input("Informe os alimentos consumidos no primeiro dia:  ")
-   
-   if alimento_usuario == "fim":
-       break
-   
-   quantidade = float(input("Quantas gramas de cada alimento?:  "))
-   
-   
-   
-   quantidade_nominal = float(alimentos[str(alimento_usuario)][1])
-   
-   x = (quantidade_nominal*quantidade)/100
-
-   print(x)
-   
-   alimentos_dia_1.append(x)
-   
-print(alimentos_dia_1)
-
-print(sum(alimentos_dia_1))
-    
+#------------------------------------------------------------------------------
